b544e5163f23b09a779edb18b8fa135360d44702
tridentmakarov <l.jungmann10@yahoo.com>
2018-09-04 17:29:19 -0400

    LOL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import { 
+	Component, 
+	ElementRef, 
+	OnInit, 
+	OnDestroy, 
+	Input, 
+	Output, 
+	EventEmitter,
+	AfterViewInit,
+	Inject, 
+	HostListener, 
+	ChangeDetectorRef,
+	ViewEncapsulation
+} from '@angular/core';
+
 import { Router, NavigationEnd } from '@angular/router';
 import { MatDialog, MatIcon, MatSnackBar } from '@angular/material';
 import * as $ from "jquery";
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 
 @Component({
@@ -9,13 +31,44 @@ import * as $ from "jquery";
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 	encapsulation: ViewEncapsulation.None,
+	animations: [
+	trigger('scrollAnimation', [
+		state('grow', style({
+			minHeight: '80px',
+			opacity: 1.0
+		})),
+		state('shrink',   style({
+			minHeight: '40px',
+			opacity: 0.5
+		})),
+		transition('grow => shrink', animate('700ms ease-in')),
+		transition('shrink => grow', animate('700ms ease-out'))
+		])
+	]
 })
 
 export class AppComponent implements OnInit, OnDestroy {
 
 	public isMobile: boolean = false;
+	public state = 'hide';
+	public toolbar = document.getElementById("toolbar");
+	public toolbarbar = document.getElementById("toolbarbar");
+	public w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	public h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	public tbW;
+	public tbH;
 
-	constructor(public snackBar: MatSnackBar){
+	private scrollingDown: boolean = true;
+	public barSize: string = "grow";
+
+	private marg: number;
+	private high: number;
+
+
+	constructor(
+		public snackBar: MatSnackBar, 
+		public el: ElementRef
+	){
 
 
 		// Check if mobile
@@ -28,6 +81,9 @@ export class AppComponent implements OnInit, OnDestroy {
 
 	ngOnInit() {
 
+		this.toolbar = document.getElementById("toolbar");
+		console.log(this.tbW, this.tbH)
+		this.toolbarbar = document.getElementById("toolbarbar");
 	}
 
 	ngOnDestroy() {
@@ -37,7 +93,29 @@ export class AppComponent implements OnInit, OnDestroy {
 	openSnackBar() {
 		console.log('opening snackbar')
 		this.snackBar.open("This is a work in progress, please come back soon!", "Okay!", {
-			duration: 800000,
+			duration: 8000,
 		});
 	}
+
+    @HostListener('window:scroll', ['$event'])
+
+
+
+    checkScroll() {
+		const componentPosition = 20;
+		const scrollPosition = window.pageYOffset;
+
+		let bottom = 600;
+		let top = 450;
+
+		if (scrollPosition >= bottom && this.scrollingDown) {
+			this.barSize = 'shrink';
+			this.scrollingDown = false;
+			console.log(this.barSize)
+		} else if (scrollPosition <= top && !this.scrollingDown ) {
+			this.barSize = "grow";
+			this.scrollingDown = true;
+			console.log(this.barSize)
+		}
+    }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component, OnInit, OnDestroy, Input, Output, EventEmitter, AfterViewInit, Inject  } from '@angular/core';
-import { ElementRef } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { ChangeDetectorRef } from '@angular/core';
+import { 
+	Component, 
+	ElementRef, 
+	OnInit, 
+	OnDestroy, 
+	Input, 
+	Output, 
+	EventEmitter,
+	AfterViewInit,
+	Inject, 
+	HostListener, 
+	ChangeDetectorRef 
+} from '@angular/core';
 
+import { Router, NavigationEnd } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatSnackBar } from '@angular/material';
@@ -10,19 +20,60 @@ import { MatSnackBar } from '@angular/material';
 import { PageScrollInstance, PageScrollService, EasingLogic } from 'ngx-page-scroll';
 import { ParallaxModule, ParallaxConfig } from 'ngx-parallax';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  group
+} from '@angular/animations';
 
 @Component({
 	selector: '',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
+	animations: [
+	trigger('scrollAnimation', [
+		// state('show', style({
+		// 	opacity: 1,
+		// 	transform: "translateX(0)"
+		// })),
+		// state('hide',   style({
+		// 	opacity: 0,
+		// 	transform: "translateX(-100%)"
+		// })),
+		transition('show => hide',  group([
+	        animate('0.3s ease', style({
+	        	transform: "translateX(0)",
+	        	opacity: 1,
+	        })),
+	        animate('0.3s 0.3s ease', style({
+		        }))
+		    ])
+		),
+		transition('hide => show', group([
+	        animate('0.3s ease', style({
+	        	transform: "translateX(-100%)",
+	          	opacity: 0,
+	        })),
+	        animate('0.3s 0.3s ease', style({
+		        }))
+		    ])
+		),
+	])]
 })
 
 
 export class HomeComponent implements OnInit {
 
 	public isMobile: boolean = false;
+	public state = 'hide';
 
-	constructor(){
+	private scrollingDown: boolean = true;
+	public barSize: string = "grow";
+
+	constructor(public el: ElementRef){
 
 
 		// Check if mobile
@@ -35,4 +86,25 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
     }
+
+    @HostListener('window:scroll', ['$event'])
+
+    checkScroll() {
+		const componentPosition = 20;
+		const scrollPosition = window.pageYOffset;
+
+		let bottom = 600;
+		let top = 450;
+
+		if (scrollPosition >= bottom && this.scrollingDown) {
+			this.barSize = 'shrink';
+			this.scrollingDown = false;
+			console.log(this.barSize)
+		} else if (scrollPosition <= top && !this.scrollingDown ) {
+			this.barSize = "grow";
+			this.scrollingDown = true;
+			console.log(this.barSize)
+		}
+    }
+}
 }
